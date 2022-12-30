ae1f8e24d6cbc2bfe8454309760c8f8939341c72
Amol Thite <amol.thite@apptware.com>
2022-12-30 14:40:27 +0530

    favicon change, welcome modal header color change, logo of aaplication changed

--- src/components/footer/FooterAdmin.tsx
+++ src/components/footer/FooterAdmin.tsx
@@ -34,40 +34,6 @@ export default function Footer() {
 					</Link>
 				</Text>
 			</Text>
-			<List display='flex'>
-				<ListItem
-					me={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={textColor} href='mailto:hello@simmmple.com'>
-						Support
-					</Link>
-				</ListItem>
-				<ListItem
-					me={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={textColor} href='https://www.simmmple.com/licenses'>
-						License
-					</Link>
-				</ListItem>
-				<ListItem
-					me={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={textColor} href='https://simmmple.com/terms-of-service'>
-						Terms of Use
-					</Link>
-				</ListItem>
-				<ListItem>
-					<Link fontWeight='500' color={textColor} href='https://www.blog.simmmple.com/'>
-						Blog
-					</Link>
-				</ListItem>
-			</List>
 		</Flex>
 	);
 }
--- src/components/footer/FooterAuth.tsx
+++ src/components/footer/FooterAuth.tsx
@@ -35,40 +35,6 @@ export default function Footer() {
 					</Link>
 				</Text>
 			</Text>
-			<List display='flex'>
-				<ListItem
-					me={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={linkColor} href='mailto:hello@simmmple.com'>
-						Support
-					</Link>
-				</ListItem>
-				<ListItem
-					me={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={linkColor} href='https://www.simmmple.com/licenses'>
-						License
-					</Link>
-				</ListItem>
-				<ListItem
-					me={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={linkColor} href='https://simmmple.com/terms-of-service'>
-						Terms of Use
-					</Link>
-				</ListItem>
-				<ListItem>
-					<Link fontWeight='500' color={linkColor} href='https://www.blog.simmmple.com/'>
-						Blog
-					</Link>
-				</ListItem>
-			</List>
 		</Flex>
 	);
 }
